268c7c0bc6de37d44fd6ed5db1874c6050a388e9
eon <noe.navarro1019@gmail.com>
2022-01-25 23:36:04 -0700

    reated CRUD endpoints for post table

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const userRoutes = require('./user-routes.js');
+const postRoutes = require('./post-routes');
 
 // direct the path to be used with API routes within userRoutes
 router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
 
 module.exports = router;
