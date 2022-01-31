757ab99226bc4cc18d12b6c62c29d2a1be3bbc8a
eon <noe.navarro1019@gmail.com>
2022-01-31 00:13:29 -0700

    finalized back end for just tech

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,9 +1,11 @@
 const router = require('express').Router();
+
 const userRoutes = require('./user-routes.js');
 const postRoutes = require('./post-routes');
+const commentRoutes = require('./comment-routes');
 
-// direct the path to be used with API routes within userRoutes
 router.use('/users', userRoutes);
 router.use('/posts', postRoutes);
+router.use('/comments', commentRoutes);
 
 module.exports = router;
